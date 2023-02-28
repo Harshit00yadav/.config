8b4498651e620a0f47f83c015b112db3905651a2
Harshit Yadav <hash00usr@gmail.com>
2023-02-28 07:53:14 +0530

    first commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("H_config.plugins-setup")
+require("H_config.core.options")
+require("H_config.core.keymaps")
+require("H_config.core.colorscheme")
+require("H_config.plugins.nvim-tree")
+require("H_config.plugins.lualine")
